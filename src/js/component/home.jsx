59da7a7698573413b264c5485e5bc7d820258b24
KrisN9 <kristinank992@gmail.com>
2022-11-09 19:29:13 +0000

    Inicio ejercicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = UseState()
+
+	const handleChange = (event) => {
+		setInputValue(event.target.value)
+	}
+
+	const handleKeyDown = (event) => {
+		console.log(event)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center black">
+			<input type='text' onChange={handleChange} onKeyDown={handleKeyDown} />
 		</div>
 	);
 };
