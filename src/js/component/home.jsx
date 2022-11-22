a5c2e056f4f0501687182b02611e08f5857c59d6
KrisN9 <kristinank992@gmail.com>
2022-11-22 20:58:08 +0000

    arreglar valor task no definido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,23 +3,48 @@ import React, {useState} from "react";
 //create your first component
 const Home = () => {
 
+	const [tasks, setTasks] = useState ([])
+
 	const [inputValue, setInputValue] = useState("")
 
-	const handleChange = (event) => {
-		setInputValue(event.target.value)
-	}
-
-	const handleKeyDown = (event) => {
-		if (event.code === "Enter") {
-			console.log(inputValue)
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (inputValue != ""){
+			let addTask = {
+				id: Math.floor(Math.random() * 1000),
+				text: inputValue,
+				completed: false
+			}
+			setTasks([...tasks, addTask])
+			setInputValue("")
 		}
 	}
 
+	const deleteTask = (id) => {
+		let filteredTasks = [...tasks].filter(task => task.id !== id);
+		setTasks(filteredTasks)
+	}
+
 	return (
-		<div className="text-center text-danger text-opacity-50">
-			<div id="demo" className="collapse show">
+		<div className="todos text-center text-danger text-opacity-50">
+			<div id="todos">
 				<h1 className="display-1">todos</h1>
-				<input type='text' value={inputValue} placeholder="No tasks, add a task" onChange={handleChange} onKeyDown={handleKeyDown} />
+				<div className="list-card">
+					<form onSubmit={handleSubmit}>
+						<input type="text" value={inputValue} onChange={event => setInputValue(event.target.value)} 
+						className="input-box" placeholder="No tasks, add a task" />
+					</form>
+					<div className="list-items">
+						{tasks.map((task) = (
+							<div className="todo" key={task.id}>
+								<p>{task.text}
+									<button className="button" onClick={() => deleteTask(task.id)}>&#10060;</button>
+								</p>
+							</div>)
+						)}
+						<p className="counter">{tasks.length > 1 ? "One task left" : "Tasks left: ${tasks.length}"}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
